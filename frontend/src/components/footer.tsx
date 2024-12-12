77c9519f357e9bf92d62ee048540f606153bec2b
Cheyenne Gomez <cheyennegomez24@gmail.com>
2024-12-12 18:55:45 -0500

    first commit

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer: React.FC = () => {
+    return (
+    <footer className="footer">
+        <br></br><p>© 2024 Recetas Navideñas </p>
+    </footer>
+    );
+};
+
+export default Footer;
